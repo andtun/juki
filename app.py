29936207192f0098bfa0f84e053044007f396a77
Andrey TTT <andtun@yandex.ru>
2017-01-15 18:06:19 +0300

    admin pg & change password

--- app.py
+++ app.py
@@ -47,9 +47,7 @@ def login():
 @post("/")
 def chklgn():
     if check_pass():
-        username = request.forms.get('username')
-        global access_level
-        access_level = access[username]
+        check_pass()
         global logged_in
         logged_in = True
         redirect("/main")
