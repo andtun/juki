1a588e97626948f006d75a12bc23f249758d60b2
Andrey TTT <andtun@yandex.ru>
2017-01-16 17:57:01 +0300

    sessions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import xlrd
 from openpyxl import load_workbook
 import beaker.middleware
-from cryptography import pycrypto
+import cryptography
 
 session_opts = {
     'session.type': 'file',
