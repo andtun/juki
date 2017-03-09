05463797401e88de15c73be7b6c25f03b5d81cfe
Andrey TTT <andtun@yandex.ru>
2017-03-09 10:46:22 +0300

    allNo

--- all_day_no.py
+++ all_day_no.py
@@ -9,13 +9,15 @@ def allNo():
     def convert(month):
         dic = {'01': "январь", '02': "февраль", '03': "март", '04': "апрель", '05': "май", '06': "июнь", '07': "июль",
                  '08': "август", '09': "сентябрь", '10': "октябрь", '11': "ноябрь", '12': "декабрь"}
-        return int(dic[month])
+        return dic[month]
 
     d = str(datetime.now())
     d = d[:d.find(" ")]
     d = d.split("-")
     month = convert(d[1]) # нужна ф-ция convert() чтоб был "месяц..."
     date = int(d[2])
+
+    print(month, date)
 
     workbook = xlrd.open_workbook('export.xlsx')
     sheet = workbook.sheet_by_index(0)
@@ -39,4 +41,7 @@ def allNo():
         sheet.cell(row=i, column=curcol).value = "H"
         book.save('export.xlsx')
 
+
+print('start')
 allNo()
+print('end')
