c9b2295b7b70db09bc9af5a926c9b6e662505055
Andrey TTT <andtun@yandex.ru>
2017-03-09 17:55:59 +0300

    alldaynoshouldwork

--- insertPoint.py
+++ insertPoint.py
@@ -40,7 +40,7 @@ list2 = ['Алеев Мурат', 'Алексеев Илья', 'Архипова
 
 
 def rdname(name):
-    name = name.decode('utf-8')
+    name = name.decode('utf8')
     print(name)
     ans = list1[list2.index(name.decode('utf8')).decode('utf8')].decode('utf8')
     return ans.decode('utf8')
