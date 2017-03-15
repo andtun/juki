acce509eb362579d78b6817b3306a17b22b5a4ca
Andrey TTT <andtun@yandex.ru>
2017-03-15 23:02:28 +0300

    import fixed

--- UserDB.py
+++ UserDB.py
@@ -49,7 +49,7 @@ def get(username):
     for i in ['username', 'pw', 'fio', 'access_level']:
         cmnd = "SELECT %s FROM Userlist WHERE username='%s';" % (i, username)
         t = str(db.fetch(cmnd))
-        d[i] = t[3:len(t)-4]
+        d[i] = t[4:len(t)-4]
     reslt = User(d['username'], d['access_level'], d['fio'], d['pw'])
     return reslt
 
@@ -83,5 +83,3 @@ add('anikina', hsh('eaani'), 'Аникина Е. А.', '10kl')
 add('zapolsky', hsh('iazap'), 'Запольский И. А.', '10kl')
 add('tiunova', hsh('mvtiu'), 'Тиунова М. В.', '10kl')
 add ('admin', hsh('adminpsw'), 'Администратор', 'admin')"""
-
-print(get('user1').fio)
