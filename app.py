f17b84817aa6f538e10a6a57de6d75696374588f
Andrey TTT <andtun@yandex.ru>
2017-03-15 23:14:06 +0300

    import fixed

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def chngpsw_process():
 @get("/check_user")
 @need_auth
 def chk_usr():
-    return UserDB.get(request.session['username']).fio.encode('Unicode')
+    return UserDB.get(request.session['username']).fio.encode()
 
 #======================================================================
 #                     ADMIN STUFF
