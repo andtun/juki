ed37da9d88eaf079cc044ebdfb88ae7114df49cf
Andrey TTT <andtun@yandex.ru>
2017-03-17 13:53:27 +0300

    all day no

--- all_day_no.py
+++ all_day_no.py
@@ -4,17 +4,14 @@ import xlrd
 from openpyxl import load_workbook
 #import warnings
 from datetime import datetime
+from insertPoint import convert
+from datetime import datetime
 
 def allNo():
-    def convert(month):
-        dic = {'01': "январь", '02': "февраль", '03': "март", '04': "апрель", '05': "май", '06': "июнь", '07': "июль",
-                 '08': "август", '09': "сентябрь", '10': "октябрь", '11': "ноябрь", '12': "декабрь"}
-        return dic[month]
-
     d = str(datetime.now())
     d = d[:d.find(" ")]
     d = d.split("-")
-    month = convert(d[1]) # нужна ф-ция convert() чтоб был "месяц..."
+    month = convert(d[1])
     date = int(d[2])
 
     workbook = xlrd.open_workbook('export.xlsx')
@@ -22,16 +19,15 @@ def allNo():
 
     for i in range(sheet.ncols):
         data = sheet.cell_value(0, i)
-        print(data)
-        if data == month.decode('utf-8'):
+        if data == month.decode("utf-8"):
             break
+
     curcol = i + date
 
-    for i in range(3,sheet.nrows+1):
-        book=load_workbook('export.xlsx')
+    for i in range(3, sheet.nrows + 1):
+        book = load_workbook('export.xlsx')
         sheet = book.active
         sheet.cell(row=i, column=curcol).value = "H"
-        print(i,curcol)
         book.save('export.xlsx')
 
 
