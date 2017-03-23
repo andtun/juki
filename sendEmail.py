3e2e51d5f4aaa13ff6a562fcb3ee8a87655e245d
Alexander Tyunyatkin <atyunyatkin@osisoft.com>
2017-03-23 15:33:34 +0300

    sendmail

--- sendEmail.py
+++ sendEmail.py
@@ -6,8 +6,8 @@ def send_simple_message():
     return requests.post(
         "https://api.mailgun.net/v3/app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org/messages",
         auth=("api", "key-1c96ae1c8fe7767ef1191d0827f41f27"),
-        data={"from": "Excited User <mailgun@app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org>",
-              "to": ["andtun@yandex.ru", "mailgun@app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org"],
+        data={"from": "Восстановление пароля int-school <noreply@int-school.herokuapp.com>",
+              "to": ["andtun@yandex.ru"],
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!"})
 
