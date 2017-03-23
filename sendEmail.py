3e4b632034715465748dab11bbe4ab2f8f1d0ad9
Alexander Tyunyatkin <atyunyatkin@osisoft.com>
2017-03-23 10:55:02 +0300

    email api

--- sendEmail.py
+++ sendEmail.py
@@ -5,7 +5,7 @@ import requests
 def send_simple_message():
     return requests.post(
         "https://api.mailgun.net/v3/app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org/messages",
-        auth=("api", "YOUR_API_KEY"),
+        auth=("api", "key-1c96ae1c8fe7767ef1191d0827f41f27"),
         data={"from": "Excited User <mailgun@app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org>",
               "to": ["andtun@yandex.ru", "mailgun@app00d6f5c2e4444a0da623dba4daad99a8.mailgun.org"],
               "subject": "Hello",
