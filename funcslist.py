82c29e425f8b00d3be46ec6fdb2ec23028edeb1b
Andrey Tyunyatkin <andtun@yandex.ru>
2017-05-01 23:45:40 +0300

    cmt

--- funcslist.py
+++ funcslist.py
@@ -179,7 +179,7 @@ def find_cell(fio, month, date, filename):        #find a cell
 
                #finding fio
     for i in range(sheet.nrows):
-        data = str(sheet.cell_value(i, 0))
+        data = str(sheet.cell_value(i, 0).encode("unicode_escape"))
         print(data)
         data = list(data.split(" "))
         data = data[0] + " " + data[1]
